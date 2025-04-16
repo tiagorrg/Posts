652912ebf4c2bdd7b68e13d3aaeb75ecef97a5be
Семен <semen@MacBook-Air-Semen.local>
2025-04-17 00:54:04 +0300

    Add basic slices

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import {
 } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+
 import { Posts } from './pages/posts/index.jsx'
 import { Root } from './components/Root/index.jsx'
 import { DetailPost } from './pages/posts/detail/index.jsx'
@@ -13,6 +15,7 @@ import { EditPost } from './pages/posts/edit/index.jsx'
 import { AddPost } from './pages/posts/add/index.jsx'
 import { Registration } from './pages/registration/index.jsx'
 import { Auth } from './pages/auth/index.jsx'
+import { store } from './redux/store.js'
 
 const router = createBrowserRouter([
   {
@@ -53,6 +56,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
