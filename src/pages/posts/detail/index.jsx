3ab99c1096063c8627d6559714e4352fc7bd9da2
Семен <semen@MacBook-Air-Semen.local>
2025-04-24 21:22:54 +0300

    Add delete post

--- src/pages/posts/detail/index.jsx
+++ src/pages/posts/detail/index.jsx
@@ -1,24 +1,37 @@
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
 import { Typo } from '../../../components/Typo'
 import { Container } from "../../../components/Container";
 import { Link } from '../../../components/Link'
-import { getPostById, showPost } from "../../../redux/slices/postsSlice";
+import { deletePost, getPostById, showPost } from "../../../redux/slices/postsSlice";
 import { useSelector, useDispatch } from "react-redux"
 
 import * as SC from './styles'
 
 export const DetailPostPage = () => {
     const { id } = useParams()
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate()
+
     const { list } = useSelector((state) => state.posts.posts)
     const postForView = useSelector((state) => state.posts.postForView)
-    const dispatch = useDispatch()
+
+    const [postForDelete, setPostForDelete] = useState(null)
+
+    const onDeletePost = () => {
+        dispatch(deletePost(postForDelete.id))
+
+        setPostForDelete(null)
+
+        navigate('/posts')
+    }
 
     useEffect(() => {
         const intId = Number(id)
         const findedPost = list ? list.find((item) => item.id === Number(id)) : undefined
 
-        if (findedPost){
+        if (findedPost) {
             dispatch(showPost(findedPost))
         } else {
             dispatch(getPostById(intId))
@@ -39,13 +52,26 @@ export const DetailPostPage = () => {
 
     return (
         <Container>
+            {postForDelete &&
+                <SC.ModalWrapper>
+                    <SC.Modal>
+                        <SC.ModalText>Вы точно уверенны, что хотите удалить элемент?</SC.ModalText>
+                        <SC.ModalContent>
+                            <SC.DeleteButton onClick={() => onDeletePost()}>Да</SC.DeleteButton>
+                            <button onClick={() => setPostForDelete(null)}>Нет</button>
+                        </SC.ModalContent>
+                    </SC.Modal>
+                </SC.ModalWrapper>
+            }
+
             <Typo>{post.title}</Typo>
             <SC.Image src={image} alt={post.title} />
             <SC.Text>{post.body}</SC.Text>
             <div style={{ clear: 'both' }} />
             <SC.LinkWrapper>
                 <Link to={'/posts/'}>Обратно к публикациям</Link>
-                <Link to={`/posts/${post.id}/edit`}>Редактировать</Link>
+                {list && <Link to={`/posts/${post.id}/edit`}>Редактировать</Link>}
+                {list && <SC.DeleteButton onClick={() => setPostForDelete(post)}>Удалить</SC.DeleteButton>}
             </SC.LinkWrapper>
         </Container>
     )
--- src/pages/posts/detail/styles.js
+++ src/pages/posts/detail/styles.js
@@ -16,4 +16,61 @@ export const LinkWrapper = styled.div`
     display: flex;
     justify-content: center;
     gap: 15px;
+    align-items: center;
+`
+
+export const DeleteButton = styled.button`
+    border: 1px solid black;
+    background: white;
+    padding: 5px 15px;
+    color: black;
+    cursor: pointer;
+    border-radius: 10px;
+
+    &:hover {
+        background-color: red;
+        color: white;
+        border-color: red;
+    }
+`
+
+export const ModalWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgb(0, 0, 0, 0.5);
+    z-index: 1;
+`
+
+export const Modal = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+    color: black;
+    border: 1px solid white;
+    padding: 15px; 
+    background-color: white;
+    display: flex;
+    gap: 15px;
+    flex-direction: column;
+    border-radius: 15px;
+`
+
+export const ModalText = styled.div`
+    color: black;
+    font-weight: bold;
+    font-size: 20px;
+    text-align: center;
+`
+
+export const ModalContent = styled.div`
+    display: flex;
+    gap: 15px;
+    align-items: center;
+    justify-content: center;
+    width: 100%
 `
