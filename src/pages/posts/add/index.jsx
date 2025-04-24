bb0acdfeac5dc4056e9482c05ea1efadfc441589
Семен <semen@MacBook-Air-Semen.local>
2025-04-24 18:34:07 +0300

    add edit post

--- src/pages/posts/add/index.jsx
+++ src/pages/posts/add/index.jsx
@@ -1,4 +1,18 @@
 import React from "react";
 import { PostForm } from "../components/PostForm";
+import { useDispatch } from "react-redux";
+import { addPost } from "../../../redux/slices/postsSlice";
 
-export const AddPostPage = () => <PostForm />
+export const AddPostPage = () => {
+    const dispatch = useDispatch()
+
+    const onSubmitForm = (formValues) => {
+        dispatch(addPost(formValues))
+    }
+
+    return (
+        <>
+            <PostForm title={'Добавление нового поста'} onSubmitForm={onSubmitForm}/>
+        </>
+    )
+}
--- src/pages/posts/components/PostForm/index.jsx
+++ src/pages/posts/components/PostForm/index.jsx
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 import { Container } from "../../../../components/Container";
 import * as SC from './styles'
 import { Typo } from "../../../../components/Typo";
-import { useDispatch } from "react-redux";
-import { addPost } from "../../../../redux/slices/postsSlice";
 
 const DEFAULT_VALUES = {title: '', body: ''}
 
-export const PostForm = () => {
-    const dispatch = useDispatch()
-    const [formValues, setFormValues] = useState(DEFAULT_VALUES)
+export const PostForm = ({title, onSubmitForm, defaultValues}) => {
+    const [formValues, setFormValues] = useState(defaultValues || DEFAULT_VALUES)
 
     const onChange = (name, value) => {
         setFormValues({...formValues, [name]: value})
@@ -17,16 +14,16 @@ export const PostForm = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-        dispatch(addPost(formValues))
+        onSubmitForm(formValues)
 
-        setFormValues(DEFAULT_VALUES)
+        !defaultValues && setFormValues(DEFAULT_VALUES)
     }
     
     const disabled = !formValues.title || !formValues.body
 
     return (
         <Container>
-            <Typo>Добавление нового поста</Typo>
+            <Typo>{title}</Typo>
             <SC.Form onSubmit={onSubmit}>
                 <SC.Field>
                     <SC.Input 
