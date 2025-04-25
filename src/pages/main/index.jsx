53c71f1257cbb9b261ff98922bba46051f72cc37
Семен <semen@MacBook-Air-Semen.local>
2025-04-26 02:41:36 +0300

    Add search and sort posts

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -8,6 +8,8 @@ import { getFreshPosts } from "../../redux/slices/postsSlice";
 import Loader from "../../components/ui/Loader";
 import { Button } from "../../components/ui/Button";
 import * as SC from "./styles"
+import { filterPosts } from "./utils";
+import { Input } from "../../components/ui/Input";
 
 const POSTS_PAGE = 9
 
@@ -22,62 +24,122 @@ export const MainPage = () => {
         right: true
     })
     const [postsPage, setPostsPage] = useState(null)
+    const [filteredPosts, setFilteredPosts] = useState(null)
+    const [defaultPosts, setDefaultPosts] = useState(null)
 
     useEffect(() => {
-        if (!posts){
+        if (!posts) {
             dispatch(getFreshPosts())
         }
+
+        setFilteredPosts(posts)
     }, [dispatch, posts])
 
     useEffect(() => {
-        if (!posts || !posts.length) {
+        if (!filteredPosts || !filteredPosts.length) {
             setDisabled({ left: true, right: true });
             return;
         }
-    
-        const totalPages = Math.ceil(posts.length / POSTS_PAGE);
+
+        const totalPages = Math.ceil(filteredPosts.length / POSTS_PAGE);
         const offset = (page - 1) * POSTS_PAGE;
-    
+
         setDisabled({
             left: page <= 1,
             right: page >= totalPages
         });
-    
-        setPostsPage(posts.slice(offset, offset + POSTS_PAGE));
-    }, [page, posts]);
+
+        setPostsPage(filteredPosts.slice(offset, offset + POSTS_PAGE));
+    }, [page, filteredPosts]);
 
     const buttonLeft = () => {
-        setPage(page - 1 )
+        setPage(page - 1)
     }
 
     const buttonRight = () => {
-        setPage(page + 1 )
+        setPage(page + 1)
+    }
+
+    const sortPosts = (direction, posts) => {
+        setPage(1)
+        return [...posts].sort((a, b) => {
+            switch (direction) {
+                case 'asc':
+                    return a.title.localeCompare(b.title);
+                case 'desc':
+                    return b.title.localeCompare(a.title);
+                default:
+                    return posts
+            }
+        })
     }
 
     return (
         <Container>
-            {loading && <Loader/>}
+            {loading && <Loader />}
             {posts &&
                 <>
                     <Typo>Свежие посты</Typo>
 
-                    <SC.PageButtonWrapper>
-                        <Button onClick={buttonLeft} disabled={disabled.left}>Left</Button>
-                        <Button onClick={buttonRight} disabled={disabled.right}>Right</Button>
-                    </SC.PageButtonWrapper>
+                    <SC.SearchAndSortWrapper>
+                        <SC.FormSearchFilter onSubmit={(e) => {
+                            e.preventDefault()
+                            setPage(1)
+                            setDefaultPosts(filterPosts(posts, e.target.search.value))
+                            setFilteredPosts(filterPosts(posts, e.target.search.value))
+                        }}>
+                            <Input type="text" placeholder="Название поста" name="search" />
+                            <Button type="submit">Найти</Button>
+                        </SC.FormSearchFilter>
 
-                    <Posts posts={postsPage}/>
+                        <div>
+                            <input
+                                type="radio"
+                                id="opt1"
+                                name="switch"
+                                onChange={() => setFilteredPosts(sortPosts('default', defaultPosts))}
+                            />
+                            <label htmlFor="opt1">Default</label>
 
-                    <SC.PageButtonWrapper>
-                        <Button onClick={buttonLeft} disabled={disabled.left}>Left</Button>
-                        <Button onClick={buttonRight} disabled={disabled.right}>Right</Button>
-                    </SC.PageButtonWrapper>
+                            <input
+                                type="radio"
+                                id="opt2"
+                                name="switch"
+                                onChange={() => setFilteredPosts(sortPosts('asc', filteredPosts))}
+                            />
+                            <label htmlFor="opt2">A-Z</label>
+
+                            <input
+                                type="radio"
+                                id="opt3"
+                                name="switch"
+                                onChange={() => setFilteredPosts(sortPosts('desc', filteredPosts))}
+                            />
+                            <label htmlFor="opt3">Z-A</label>
+                        </div>
+                    </SC.SearchAndSortWrapper>
+
+                    {filteredPosts && filteredPosts.length > 0
+                        ? <>
+                            <SC.PageButtonWrapper>
+                                <Button onClick={buttonLeft} disabled={disabled.left}>Left</Button>
+                                <Button onClick={buttonRight} disabled={disabled.right}>Right</Button>
+                            </SC.PageButtonWrapper>
+
+                            <Posts posts={postsPage} />
+
+                            <SC.PageButtonWrapper>
+                                <Button onClick={buttonLeft} disabled={disabled.left}>Left</Button>
+                                <Button onClick={buttonRight} disabled={disabled.right}>Right</Button>
+                            </SC.PageButtonWrapper>
+                        </>
+                        : <SC.TextUnFind>Посты не найдены</SC.TextUnFind>}
                 </>
             }
             {post &&
                 <>
                     <Typo>Последний просмотренный пост</Typo>
-                    <Posts posts={[post]}/>
+                    <Posts posts={[post]} />
                 </>
             }
         </Container>
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,8 +1,27 @@
 import styled from "styled-components";
 
+export const FormSearchFilter = styled.form`
+    display: flex;
+    max-width: 30%;
+    gap: 5px;
+`
+
+export const SearchAndSortWrapper = styled.div`
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
 export const PageButtonWrapper = styled.div`
     display: flex;
     gap: 15px;
     justify-content: center;
-    margin-bottom: 20px
+    margin-bottom: 20px;
+`
+
+export const TextUnFind = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 50px;
 `
